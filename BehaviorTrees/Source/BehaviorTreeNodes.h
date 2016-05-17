aa505d918d050e6777a90b7c188ad3e861117303
MidnightDrifter <linkmasterchief@aol.com>
2016-05-17 14:59:05 -0700

    Added files I made earlier woo

--- BehaviorTrees/Source/BehaviorTreeNodes.h
+++ BehaviorTrees/Source/BehaviorTreeNodes.h
@@ -3,13 +3,24 @@
 #ifndef BEHAVIOR_TREE_NODES_H
 #define BEHAVIOR_TREE_NODES_H
 
-
+//Selectors
+#include "Nodes/SelectorWeightedRandom.h"  //SH added selecter
 #include "Nodes/SelectorRandom.h"
 #include "Nodes/SelectorSequencer.h"
+
+
+//Decorators
 #include "Nodes/DecoratorInverter.h"
 #include "Nodes/DecoratorRunUntilSucceed.h"
+#include "Nodes/DecoratorRunXTimes.h"  //SH added decorator
+#include "Nodes/DecoratorRunUntilFail.h"  //SH added decorator
+#include "Nodes/DecoratorRunXTimesOrUntilSucceed.h"  //SH added decorator
+
+
+//General leaf nodes
 #include "Nodes/PathToTarget.h"
 #include "Nodes/SelectRandomTarget.h"
 #include "Nodes/SelectFurthestTarget.h"
+#include "Nodes/SelectWeightedRandomTarget.h"  //SH added leaf node
 
 #endif
